1ec1cf77f80156ff6142a0a8f199d06ab9633fad
shoponline12 <shoponlinecase2023@gmail.com>
2026-07-29 20:09:41 +0300

    Refactor server.js to include payment routes

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 
+const paymentRoutes = require("./routes/payment");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("myPOS Server is running!");
+    res.send("myPOS Server is running!");
 });
+
+app.use("/payment", paymentRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
