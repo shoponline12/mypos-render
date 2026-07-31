8c52e2dc709ace2d98d3b967b576f301cdc871f4
shoponline12 <shoponlinecase2023@gmail.com>
2026-07-31 16:26:25 +0300

    Update checkout.js

--- routes/checkout.js
+++ routes/checkout.js
@@ -2,7 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("checkout");
+    res.render("checkout", {
+        cart: []
+    });
+});
+
+router.post("/", (req, res) => {
+
+    console.log("SHOPIFY CART:");
+    console.log(req.body);
+
+    res.render("checkout", {
+        cart: req.body.items || []
+    });
+
 });
 
 module.exports = router;
