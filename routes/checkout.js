40f4df8eb7725dbb0af7def646f056478c7665ba
shoponline12 <shoponlinecase2023@gmail.com>
2026-07-31 19:54:38 +0300

    Update checkout.js

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,28 +1,13 @@
-const express = require("express");
-
-const router = express.Router();
-
-let currentCart = null;
-
 router.post("/", (req, res) => {
 
+    console.log(req.body);
+
     currentCart = JSON.parse(req.body.cart);
+
+    console.log(currentCart);
 
     res.json({
         success: true
     });
 
 });
-
-router.get("/", (req, res) => {
-
-    res.render("checkout", {
-        cart: currentCart || {
-            items: [],
-            total_price: 0
-        }
-    });
-
-});
-
-module.exports = router;
