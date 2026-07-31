1d0d180b921b68abe4bda7618a7001d4827281e6
shoponline12 <shoponlinecase2023@gmail.com>
2026-07-31 16:33:14 +0300

    Update cart.js

--- routes/cart.js
+++ routes/cart.js
@@ -1,13 +1,18 @@
 const express = require("express");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const response = await fetch("https://summer-shop.eu/cart.js");
+    const cart = await response.json();
 
-    res.json({
-        success: true,
-        items: []
+    res.json(cart);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
     });
-
+  }
 });
 
 module.exports = router;
