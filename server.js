8582fef8f1da501ef9b42a23ebeffbdaf9961527
shoponline12 <shoponlinecase2023@gmail.com>
2026-07-31 15:54:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const paymentRoutes = require("./routes/payment");
 const notifyWebhook = require("./webhooks/notify");
+const checkoutRoute = require("./routes/checkout");
 
 const app = express();
 
@@ -13,6 +16,11 @@ function rawBodySaver(req, res, buf, encoding) {
   }
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(cors());
 app.use(express.json({ verify: rawBodySaver }));
 app.use(express.urlencoded({ extended: true, verify: rawBodySaver }));
@@ -20,6 +28,8 @@ app.use(express.urlencoded({ extended: true, verify: rawBodySaver }));
 app.get("/", (req, res) => {
   res.send("myPOS Server is running!");
 });
+
+app.use("/checkout", checkoutRoute);
 
 app.use("/payment", paymentRoutes);
 app.use("/payment", notifyWebhook);
